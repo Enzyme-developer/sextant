8366543180be747de8f4203f214bed7cb0bf4d04
Ayoe <anomalous189@gmail.com>
2022-07-11 21:51:12 +0100

    first commit

---
+++ src/components/Exhibit.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Child from './Child'
+
+const Exhibit = () => {
+  return (
+    <div>
+        <h1>heading</h1>
+        <Child />
+    </div>
+  )
+}
+
+export default Exhibit
